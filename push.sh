4a6d2ea7ff0e4e52d3709bb36aedef5e7cb6f2de
awadhesh14 <dapst8@gmail.com>
2019-10-12 11:25:00 +0530

    first

---
+++ push.sh
@@ -0,0 +1,6 @@
+git remote add origin https://github.com/awadhesh14/awadhesh14.github.io.git
+git remote -v
+git push --set-upstream origin master
+
+PLEASE ADD ANSWER TO TODO
+https://stackoverflow.com/questions/21072687/fatal-no-configured-push-destination/21073767
